a0ba812a2367c6ee933996eb8b66d0d25515ce93
Anand Chitipothu <anandology@gmail.com>
2023-05-09 16:21:40 +0530

    Added a check for the first task

--- checks.py
+++ checks.py
@@ -1,11 +1,14 @@
-from capstone_checker import register_check, main
-
+from capstone_checker import register_check, main, ValidationError
+import subprocess
 
 @register_check()
-def custom_check(context, args):
-    # context: {"app_url": "...", "app_dir": "..."}
-    # args: {"a": 1, "b": 2}  specified in capstone.yml
-    pass
+def check_command(context, args):
+    print(context, args)
+    command = args['command']
+    p = subprocess.Popen(command, shell=True, cwd=context['app_dir'])
+    p.wait(timeout=2)
+    if p.returncode != 0:
+        raise ValidationError(f"Command {command} failed with non-zero exit status. ({p.returncode})")
 
 
 if __name__ == "__main__":
